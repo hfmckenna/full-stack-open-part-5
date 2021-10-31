08d2c3c460e6a204846c90b37ccfaac65eb9dc28
hfmckenna <hfmckenna@outlook.com>
2021-10-31 16:36:20 +0000

    5.7 Complete

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [blogDetailDisplay, setBlogDetailDisplay] = useState(false);
+  const toggleView = () => {
+    setBlogDetailDisplay(current => !current);
+  };
+  const addLike = () => {};
+
+  return (
+    <div style={{ border: '2px solid black', marginBottom: '8px' }}>
+      <p>
+        {blog.title} {blog.author}
+        <button onClick={toggleView}>view</button>
+      </p>
+      <div style={blogDetailDisplay ? {display: 'inline'} : {display: 'none'}}>
+        <p>{blog.url}</p>
+        <p>
+          likes {blog.likes}
+          <button onClick={addLike}>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
